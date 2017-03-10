21f10838835117f05ef27ddf138d3aef9197dc8a
johnsshipp <johnsshipp@Johns-MacBook-Air-2.local>
2017-03-10 13:26:48 -0600

    Added login handling php file
    Added login.php to handle username and password. Also includes
    protections against malicious script injections. No password encryption
    yet

--- homePage/test.js
+++ homePage/test.js
@@ -4,7 +4,6 @@ function populatePage(k){
     populateSideBar(k);
   }
 }
-//Test comment addition
 function populatePageGrid(num){
   for(var k = 0;k<num;k++){
     var outerDiv=document.createElement("DIV");
