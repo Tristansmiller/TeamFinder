e69cdbb585bf087e027778ef8de0504139d76b3f
Tristan Miller <tristansmiller@gmail.com>
2017-03-24 16:08:14 -0500

    Refactored javascript files
    Moved portions of the javascript code that edits html docs into its own
    functions that can then be called.

--- accountPage/friends.js
+++ accountPage/friends.js
@@ -1,55 +1,125 @@
 function populateFriends(num){
   for(var k=1;k<=num;k++){
-    var friendBlock=document.createElement("DIV");
-    var att = document.createAttribute("class");
-    att.value="friend-block";
-    friendBlock.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value=("friend-block"+k);
-    friendBlock.setAttributeNode(att);
-    friendBlock.style.top=(""+(27.5*(k-1)+5)+"%")
+    var friendBlock = createDiv("friend-block",k);
 
-    var leftQuarterDiv = document.createElement("DIV");
-    att = document.createAttribute("class");
-    att.value = "left-quarter-block";
-    leftQuarterDiv.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("left-quarter-block"+k);
-    leftQuarterDiv.setAttributeNode(att);
-
-    var avatarImg = document.createElement("IMG");
-    att = document.createAttribute("class");
-    att.value = "friend-avatar";
-    avatarImg.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("friend-avatar"+k);
-    avatarImg.setAttributeNode(att);
-    att = document.createAttribute("src");
-    att.value = ("assets/testAvatar.png");
-    avatarImg.setAttributeNode(att);
-
-    var friendName = document.createElement("P");
-    att = document.createAttribute("class");
-    att.value = "friend-name";
-    friendName.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("friend-name"+k);
-    friendName.appendChild(document.createTextNode("Test Name"));
-
-    var friendBio = document.createElement("P");
-    att = document.createAttribute("class");
-    att.value = "friend-bio";
-    friendBio.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("friend-bio"+k);
-    friendBio.setAttributeNode(att);
-    friendBio.appendChild(document.createTextNode("Test Bio"));
-
-    leftQuarterDiv.appendChild(avatarImg);
-    leftQuarterDiv.appendChild(friendName);
+    var leftQuarterDiv = createDiv("left-quarter-block",k);
     friendBlock.appendChild(leftQuarterDiv);
+
+    var avatarImg = createImage("friend-avatar","assets/testAvatar.png",k);
+    leftQuarterDiv.appendChild(avatarimg);
+
+    var friendName = createParagraph("friend-name","TEST NAME",k);
+    leftQuarterDiv.appendChild(friendName);
+
+    var friendBio = createParagraph("friend-bio","TEST BIO",k);
     friendBlock.appendChild(friendBio);
 
     document.getElementById("friends-list").appendChild(friendBlock);
   }
+}
+
+function createDiv(name,num){
+  var div=document.createElement("DIV");
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  div.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  div.setAttributeNode(att);
+
+  if(name=="friend-block")
+    div.style.top=(""+(27.5*(k-1)+5)+"%");
+  return div;
+}
+
+function createLink(name,path,num){
+  var link = document.createElement("A");
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  link.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=name+""+num;
+  link.setAttributeNode(att);
+
+  att = document.createAttribute("href");
+  att.value = path;
+  link.setAttributeNode(att);
+
+  return link;
+}
+
+function createImage(name,path,num){
+  var img = document.createElement("IMG");
+
+  var att = document.createAttribute("class");
+  att.value = name;
+  img.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value = name+""+num;
+  img.setAttributeNode(att);
+
+  att = document.createAttribute("src");
+  att.value = path;
+  img.setAttributeNode(att);
+
+  return img;
+}
+function createParagraph(name,content,num){
+  var para = document.createElement("P");
+
+  var innerText = document.createTextNode(content);
+  para.appendChild(innerText);
+
+  att = document.createAttribute("class");
+  att.value=name;
+  para.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  para.setAttributeNode(att);
+
+  return para;
+
+}
+
+function createHeader(name,content,hNum,num){
+  var header = document.createElement("H"+hNum);
+
+  var innerText = document.createTextNode(content);
+  header.appendChild(innerText);
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  header.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  header.setAttributeNode(att);
+
+  return header;
+}
+
+function createButton(name,content,num){
+  var but = document.createElement("BUTTON");
+
+  att = document.createAttribute("class");
+  att.value = "btn btn-apply btn-primary "+name;
+  but.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value = name+""+num;
+  but.setAttributeNode(att);
+
+  att = document.createAttribute("type");
+  att.value = "button";
+  but.setAttributeNode(att);
+
+  but.textContent = content;
+
+  return but;
 }
--- homePage/test.js
+++ homePage/test.js
@@ -1,10 +1,3 @@
-function populatePage(k){
-  for(var k=0;k<num;k++){
-    populatePageGrid(k);
-    populateSideBar(k);
-  }
-}
-<<<<<<< HEAD
 function loggedAs(name) {
     var user = name;
     var p = document.createElement("P");
@@ -15,120 +8,147 @@ function loggedAs(name) {
     p.setAttributeNode(att);
     document.getElementById("currentUserBox").appendChild(p);
 }
+
 function searchPage(num, filter) {
   for(var k = 0; k < num; k++) {
     var ele = document.getElementById("gridCell"+k);
     var desc = document.getElementById("cellDesc"+k).innerHTML;
     if (filter=="") {ele.style.display = "flex"}
-    else if(!desc.toLowerCase().includes(filter.toLowerCase())) {ele.style.display = "none";} 
+    else if(!desc.toLowerCase().includes(filter.toLowerCase())) {ele.style.display = "none";}
     else ele.style.display = "flex";
   }
 }
-function populatePageGrid(num){
-  for(var k = 0;k<num;k++){
-      var outerDiv=document.createElement("DIV");
-      var att = document.createAttribute("class");
-      att.value="gridCell";
-      outerDiv.setAttributeNode(att);
-      att = document.createAttribute("id");
-      att.value=("gridCell"+k);
-      outerDiv.setAttributeNode(att);
 
-      var imgLink = document.createElement("A");
-      att = document.createAttribute("href");
-      att.value = "../gamePage/index.html";
-      imgLink.setAttributeNode(att);
-      var img = document.createElement("IMG");
-      att = document.createAttribute("class");
-      att.value = "cellImage";
-      img.setAttributeNode(att);
-      att = document.createAttribute("id");
-      att.value = ("cellImage"+k);
-      img.setAttributeNode(att);
-      att = document.createAttribute("src");
-      if(k%2==0) att.value = "assets/WoW.png";
-      else att.value = "assets/CSGO.png";
-      img.setAttributeNode(att);
-      imgLink.appendChild(img);
-
-      var para = document.createElement("P");
-      var content;
-      if(k%2==0) content = document.createTextNode("TEST DESC");
-      else content = document.createTextNode("NAME");
-      para.appendChild(content);
-      att = document.createAttribute("class");
-      att.value="cellDesc";
-      para.setAttributeNode(att);
-      att = document.createAttribute("id");
-      att.value=("cellDesc"+k);
-      para.setAttributeNode(att);
-=======
 function populatePageGrid(array){
   for(var k = 0;k<array.length;k++){
-    var outerDiv=document.createElement("DIV");
-    var att = document.createAttribute("class");
-    att.value="gridCell";
-    outerDiv.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value=("gridCell"+k);
-    outerDiv.setAttributeNode(att);
+    var outerDiv = createDiv("gridCell",k);
 
-    var imgLink = document.createElement("A");
-    att = document.createAttribute("href");
-    att.value = "../gamePage/index.html";
-    imgLink.setAttributeNode(att);
-    var img = document.createElement("IMG");
-    att = document.createAttribute("class");
-    att.value = "cellImage";
-    img.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("cellImage"+k);
-    img.setAttributeNode(att);
-    att = document.createAttribute("src");
-    att.value = array[k].picture;
-    img.setAttributeNode(att);
+    var imgLink = createLink("cellImageLink","../gamePage/index.html",k);
+    var img = createImage("cellImage",array[k].picture,k);
     imgLink.appendChild(img);
+    outerDiv.appendChild(imgLink);
 
-    var para = document.createElement("P");
-    var content = document.createTextNode(array[k].name);
-    para.appendChild(content);
-    att = document.createAttribute("class");
-    att.value="cellDesc";
-    para.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value=("cellDesc"+k);
-    para.setAttributeNode(att);
->>>>>>> refs/remotes/origin/Jacob
+    outerDiv.appendChild(document.createElement("BR"))//creates a line break to align elements properly
 
-      outerDiv.appendChild(imgLink);
-      outerDiv.appendChild(document.createElement("BR"));
-      outerDiv.appendChild(para);
-      document.getElementById("pageGrid").appendChild(outerDiv);
+    var para = createParagraph("cellDesc",array[k].name,k);
+    outerDiv.appendChild(para);
+
+    document.getElementById("pageGrid").appendChild(outerDiv);
   }
 }
 
+function createDiv(name,num){
+  var div=document.createElement("DIV");
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  div.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  div.setAttributeNode(att);
+
+  if(name=="listNode")
+    div.style.top=""+(250*num)+"px";
+  return div;
+}
+
+function createLink(name,path,num){
+  var link = document.createElement("A");
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  link.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=name+""+num;
+  link.setAttributeNode(att);
+
+  att = document.createAttribute("href");
+  att.value = path;
+  link.setAttributeNode(att);
+
+  return link;
+}
+
+function createImage(name,path,num){
+  var img = document.createElement("IMG");
+
+  var att = document.createAttribute("class");
+  att.value = name;
+  img.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value = name+""+num;
+  img.setAttributeNode(att);
+
+  att = document.createAttribute("src");
+  att.value = path;
+  img.setAttributeNode(att);
+
+  return img;
+}
+function createParagraph(name,content,num){
+  var para = document.createElement("P");
+
+  var innerText = document.createTextNode(content);
+  para.appendChild(innerText);
+
+  att = document.createAttribute("class");
+  att.value=name;
+  para.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  para.setAttributeNode(att);
+
+  return para;
+
+}
+
+function createHeader(name,content,hNum,num){
+  var header = document.createElement("H"+hNum);
+
+  var innerText = document.createTextNode(content);
+  header.appendChild(innerText);
+
+  var att = document.createAttribute("class");
+  att.value=name;
+  header.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value=(name+""+num);
+  header.setAttributeNode(att);
+
+  return header;
+}
+
+function createButton(name,content,num){
+  var but = document.createElement("BUTTON");
+
+  att = document.createAttribute("class");
+  att.value = "btn btn-apply btn-primary "+name;
+  but.setAttributeNode(att);
+
+  att = document.createAttribute("id");
+  att.value = name+""+num;
+  but.setAttributeNode(att);
+
+  att = document.createAttribute("type");
+  att.value = "button";
+  but.setAttributeNode(att);
+
+  but.textContent = content;
+
+  return but;
+}
 function populateSideBar(num){
   for(var k=0;k<num;k++){
-    var outerDiv=document.createElement("DIV");
-    var att = document.createAttribute("class");
-    att.value="feedBox";
-    outerDiv.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value=("feedbox"+k);
-    outerDiv.setAttributeNode(att);
+    var outerDiv= createDiv("feedBox",k);
 
-    var para = document.createElement("P");
-    var content = document.createTextNode("TEST");
-    para.appendChild(content);
-    att = document.createAttribute("class");
-    att.value="feedBoxContent";
-    para.setAttributeNode(att);
-    att = document.createAttribute("id");
-    att.value = ("feedBoxContent"+k);
-    para.setAttributeNode(att);
-
-
+    var para = createParagraph("feedBoxContent","TEST",k);
     outerDiv.appendChild(para);
+
     document.getElementById("sideFeed").appendChild(outerDiv);
   }
 }
