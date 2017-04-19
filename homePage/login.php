d092bce601f4192a6ccee9a2fbfbc97403da3622
johnsshipp <johnsshipp@Johns-Air-2.wlan.lsu.edu>
2017-04-18 19:23:48 -0500

    Re-working SignUp.php

--- homePage/login.php
+++ homePage/login.php
@@ -1,18 +1,45 @@
 <?php
-// define variables and set to empty values
-$usrname = $psw = ;
-if (isset($_POST["submit"])){
-       $usrname = test_input($_POST["usrname"]);
-       $psw = test_input($_POST["psw"]);
-       //echos for testing received input, remove later.
-        echo "<h2>Your Input:</h2>";
-        echo $usrname;
-        echo "<br>";
-        echo $psw;
-    }
-function test_input($data) {
+  $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "teamfinder";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	// Select all games from the database
+	$sql = "SELECT userID FROM teamfinder.users WHERE username = '".$username."' AND pass = '".$password."'";
+	$queryResult = $conn->query($sql);
+	if ($queryResult!=NULL) {
+		// output data of each row
+		while($row = $queryResult->fetch_assoc()) {
+			$currentUser = $row['userID'];
+		}
+		session_start();
+		$_SESSION['currentUser']=$currentUser;
+		header('Location: index.php');
+	}
+  else {
+		echo "0 results";
+	}
+	header('Location: index.php');
+  /*
+  function test_input($data) {
    $data = trim($data);
    $data = stripslashes($data);
    $data = htmlspecialchars($data);
-   return $data;
-}
+  return $data;
+  }
+  $usrname = test_input($_POST['usrname']);
+  $psw = test_input($_POST['psw']);
+  */
+  //echos for testing received input, remove later.
+	echo "<h2>Wrong username and/or password:</h2>";
+	echo $username;
+	echo "<br>";
+	echo $password;
+?>
