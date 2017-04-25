b268882d20621b838646aa24924deb5d88657f2a
johnsshipp <johnsshipp@Johns-MacBook-Air-2.local>
2017-04-24 20:45:03 -0500

    Added verification and re-direct after successful login

--- homePage/signUp.php
+++ homePage/signUp.php
@@ -5,6 +5,7 @@
 	$dbname = "teamfinder";
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
+
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
@@ -12,5 +13,13 @@
   $username = $_POST['username'];
 	$password = $_POST['password'];
   $email = $_POST['email'];
-  $sql = "INSERT INTO teamfinder.users (`username`, `email`, `pass`) VALUES ($username,$email,$password)";
-  ?>
+  $sql = "INSERT INTO teamfinder.users (`username`, `email`, `pass`) VALUES ('$username','$email','$password')";
+
+  if ($conn->query($sql) === TRUE) {
+    header("Location: index.html");
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+?>
